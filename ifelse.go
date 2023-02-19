616bfa9bfda446303f7ea12ba328180bd48ba5b0
Arafat5682 <arafathossain3066@gmail.com>
2023-02-19 23:50:11 +0600

    first commit

---
+++ ifelse.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+    "fmt"
+)
+
+func main(){
+  
+fmt.Print("Enter your age: ")
+var age int
+fmt.Scanf("%d", &age)
+
+if age < 3 {
+fmt.Println("Infant")
+
+}else if age>2 && age<13{
+ 
+fmt.Println("Children")
+}else if age>12 && age<=19{
+ fmt.Println("Teen age")
+}else{
+   fmt.Println("Adult")
+}
+ 
+}
